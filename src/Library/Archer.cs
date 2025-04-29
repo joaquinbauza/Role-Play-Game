95de6587ebe8564f4b8425b038011f49a37db1aa
unknown <joaquin.bauza@correo.ucu.edu.uy>
2025-04-29 19:30:16 -0300

    terminado con la parte 3, falta el test que esta en proceso

--- src/Library/Archer.cs
+++ src/Library/Archer.cs
@@ -3,14 +3,14 @@ namespace Library;
 public class Archer
 {
     public string Name { get; set; }
-    public int Life { get; set; }
+    public int Health { get; set; }
     public  Gun gun { get; set; }
     public Outfit outfit { get; set; }
 
-    public Archer(string name, int life, Gun gun, Outfit outfit)
+    public Archer(string name, int health, Gun gun, Outfit outfit)
     {
         Name = name;
-        Life = life;
+        Health = health;
         gun = gun;
         outfit = outfit; 
     }
@@ -18,13 +18,34 @@ public class Archer
     public void ChangeGun(Gun new_gun)
     {
         gun = new_gun;
-        Console.WriteLine($"Ha cambiado de {gun} a {new_gun} ");
+        Console.WriteLine($"Ha cambiado de {gun} a {new_gun} "); //muestra el cambio de arma que se realizó
     }
 
     public void ChangeOutfit(Outfit new_Outfit)
     {
         outfit = new_Outfit;
-        Console.WriteLine($"El nuevo outfit del Arquero es {new_Outfit}");
+        Console.WriteLine($"El nuevo outfit del Arquero es {new_Outfit}"); //muestra el cambio de outfit que se realizó
+    }
+
+    public int GetTotalAttack()
+    {
+        Console.WriteLine($"El ataque realizado fue: "); //para ver cuanto daño total realizado con su arma 
+        return gun.Attack_Damage;
+    }
+
+    public int GetTotalDefense()
+    {
+        Console.WriteLine($"El ataque recibido fue: "); //para ver cuanto daño defendió con su armadura
+        return outfit.Attack_Defense;
+    }
+
+    public void DamageCharacter(int Attack_Damage) // para cuando recibe el daño, ver la salud que le queda al personaje
+    {
+        Health -= Attack_Damage;
+        if (Health < 0)
+        {
+            Health = 0;
+        }
     }
 }
 
--- test/LibraryTests/ExampleTest.cs
+++ test/LibraryTests/ExampleTest.cs
@@ -1,10 +1,20 @@
 namespace LibraryTests;
+using Library;
 
 public class Tests
 {
+    public Archer archer1;
+    public Archer archer2;
     [SetUp]
     public void Setup()
     {
+        Gun gun1 = new Gun("Arco", 10);
+        Outfit outfit1 = new Outfit("Capa", 5);
+        Archer archer1 = new Archer("jota", 100, gun1, outfit1);
+
+        Gun gun2 = new Gun("Ballesta", 7);
+        Outfit outfit2 = new Outfit("Traje", 10);
+        Archer archer2 = new Archer("bolso gallina", 70, gun2, outfit2);
     }
 
     [Test]
