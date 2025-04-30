af46431b297159dbda98a2856ddb01c6546993c3
unknown <joaquin.bauza@correo.ucu.edu.uy>
2025-04-30 19:06:27 -0300

    Arquero creado, con su program

--- src/Library/Archer.cs
+++ src/Library/Archer.cs
@@ -1,51 +1,15 @@
+using System.Reflection.PortableExecutable;
+
 namespace Library;
 
-public class Archer
+public class Archer : Personaje
 {
-    public string Name { get; set; }
-    public int Health { get; set; }
-    public  Weapon Weapon { get; set; }
-    public Armour Armour { get; set; }
-
-    public Archer(string name, int health, Weapon weapon, Armour armour)
+    public Archer(string name, int health, int attack)
     {
-        Name = name;
-        Health = health;
-        Weapon = weapon;
-        Armour = armour; 
-    }
-
-    public void ChangeGun(Weapon newWeapon)
-    {
-        Weapon = newWeapon;
-        Console.WriteLine($"Ha cambiado de {Weapon} a {newWeapon} "); //muestra el cambio de arma que se realizó
-    }
-
-    public void ChangeOutfit(Armour newArmour)
-    {
-        Armour = newArmour;
-        Console.WriteLine($"El nuevo outfit del Arquero es {newArmour}"); //muestra el cambio de outfit que se realizó
-    }
-
-    public int GetTotalAttack()
-    {
-        Console.WriteLine($"El ataque realizado fue: "); //para ver cuanto daño total realizado con su arma 
-        return Weapon.Attack_Damage;
-    }
-
-    public int GetTotalDefense()
-    {
-        Console.WriteLine($"El ataque recibido fue: "); //para ver cuanto daño defendió con su armadura
-        return Armour.Attack_Defense;
-    }
-
-    public void DamageCharacter(int Attack_Damage, int Attack_Defense) // para cuando recibe el daño, ver la salud que le queda al personaje
-    {
-        Health -= Attack_Damage + Attack_Defense;
-        if (Health < 0)
-        {
-            Health = 0;
-        }
+        this.Name = name;
+        this.Health = health;
+        this.AttackDamage = attack;
+        this.InitialHealth = health;
     }
 }
 
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System; 
+using Library;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Archer jota = new Archer("jota", 120, 30);
+            Archer baez = new Archer("baez", 120, 30);
+
+            Elemento arco = new Elemento("Arco", 15, 5);
+            Elemento capa = new Elemento("Capa", 0, 15);
+
+            Elemento arco_encantado = new Elemento("Arco encantado", 20, 10);
+            Elemento tunica = new Elemento("Tunica", 0, 10);
+
+            jota.Inventory.Add(arco);
+            jota.Inventory.Add(capa);
+
+            baez.Inventory.Add(arco_encantado);
+            baez.Inventory.Add(tunica);
+
+            jota.ShowElements();
+            baez.ShowElements();
+
+            jota.Attack(baez);
+            baez.Attack(jota);
+            jota.Attack(baez);
+        }
+    }
+}
