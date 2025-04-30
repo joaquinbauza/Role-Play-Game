f17d6b22bb370729ff664fc81f8395de28ea85c3
Joaquin Caceres <JOAQUIN.CACERES@correo.ucu.edu.uy>
2025-04-30 19:13:46 -0300

    Elfo creo que hecho

--- src/Library/Elves.cs
+++ src/Library/Elves.cs
@@ -1,50 +1,13 @@
-namespace Library;
-
-public class Elves
+namespace Library
 {
-    public string Name { get; set; }
-    public int Health { get; set; }
-    public  Weapon Weapon { get; set; }
-    public Outfit Outfit { get; set; }
-
-    public Elves(string name, int health, Weapon weapon, Outfit outfit)
-    {
-        Name = name;
-        Health = health;
-        Weapon = weapon;
-        Outfit = outfit; 
-    }
-
-    public void ChangeGun(Weapon newWeapon)
-    {
-        Weapon = newWeapon;
-        Console.WriteLine($"Ha cambiado de {Weapon} a {newWeapon} "); //Cambio de arma
-    }
-
-    public void ChangeOutfit(Outfit new_Outfit)
-    {
-        Outfit = new_Outfit;
-        Console.WriteLine($"El nuevo outfit del Elfo es {new_Outfit}"); //Cambio de ropa
-    }
-
-    public int GetTotalAttack()
-    {
-        Console.WriteLine($"El ataque realizado fue: "); //Daño del arma 
-        return Weapon.AttackDamage;
-    }
-
-    public int GetTotalDefense()
-    {
-        Console.WriteLine($"El ataque recibido fue: "); //Armadura
-        return Outfit.AttackDefense;
-    }
-
-    public void DamageCharacter(int Attack_Damage) //Daño - salud
-    {
-        Health -= Attack_Damage;
-        if (Health < 0)
+        public class Elves : Personaje
         {
-            Health = 0;
-        }
-    }
+                public Elves(string nombre, int vida, int ataque)
+                {
+                        this.Name = nombre;
+                        this.Health = vida;
+                        this.AttackPoints = ataque;
+                        this.InitialHealth = vida;
+                }
+        }        
 }
--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,1 +1,33 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using Library;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elves Daniel = new Elves("Daniel", 100, 70);
+            Elves Manuel = new Elves("Manuel", 120, 30);
+            
+            Elemento Daga = new Elemento("Daga", 10, 0);
+            Elemento Pechera = new Elemento("Pechera", 0, 15);
+            
+            Elemento Espada = new Elemento("Espada", 15, 2);
+            Elemento Ropa = new Elemento("Ropa", 0, 8);
+
+            Daniel.Inventory.Add(Daga);
+            Daniel.Inventory.Add(Pechera);
+            
+            Manuel.Inventory.Add(Espada);
+            Manuel.Inventory.Add(Ropa);
+
+            Daniel.ShowElements();
+            Manuel.ShowElements();
+
+            Daniel.Attack(Manuel);
+            Manuel.Attack(Daniel);
+            Daniel.Attack(Manuel);
+        }
+    }
+}
