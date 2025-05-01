fc1b665b78c6e5b247c896dcb01bda8df565e91a
Christian Curbelo <122582434+Christian-Curbelo-004@users.noreply.github.com>
2025-05-01 00:00:43 -0300

    Update Elemento.cs

--- src/Library/Elemento.cs
+++ src/Library/Elemento.cs
@@ -1,14 +1,13 @@
 namespace Library;
 
-<<<<<<< HEAD
+
 
 public class Elemento: IElemento 
 {
-    
-=======
+
 public class Elemento: IElemento
 {
->>>>>>> 054b1551bddccbd560fd5d20d5271091588da943
+
     public string Name {get; private set;}
     public int Attack {get; private set;}
     public int Armor {get; private set;}
